1ce8fbce34a62808769c6a27ae87844c5672f8fb
ABskr <bskr26@gmx.de>
2021-04-19 10:08:56 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import productsRoute from "./products/index.js"
+import reviewsRoute from "./reviews/index.js"
+
+const server = express()
+const port = process.env.PORT || 5000
+const pathToPublic = join(dirname(fileURLToPath(import.meta.url), "../public/img"))
+
+server.use(express.static(pathToPublic))
+server.use(cors())
+server.use(express.json())
+
+server.use("/products", productsRoute)
+server.use("/products/reviews", reviewsRoute)
+
+server.listen(port, () => console.log("Server is running on port: ", port))
+server.on("error", (error) => console.log("Server is not running ", error))
