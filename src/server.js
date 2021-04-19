72f41db1a4549ad1955916cfdcb8c7dbceebb2eb
ABskr <bskr26@gmx.de>
2021-04-19 10:32:18 +0200

    errorHandler integrated in server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import listEndpoints from 'express-list-endpoints'
 import cors from 'cors'
 import productsRoute from "./products/index.js"
 import reviewsRoute from "./reviews/index.js"
+import {join, dirname} from 'path'
+import {
+  error404Handler, error401Handler, error403Handler, genericErrorHandler
+} from "./middlewares/errorHandler.js"
 
 const server = express()
 const port = process.env.PORT || 5000
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Logged ${req.url} ${req.method} -- ${new Date()}`)
+  next()
+}
+
 const pathToPublic = join(dirname(fileURLToPath(import.meta.url), "../public/img"))
 
 server.use(express.static(pathToPublic))
@@ -14,5 +24,13 @@ server.use(express.json())
 server.use("/products", productsRoute)
 server.use("/products/reviews", reviewsRoute)
 
+//ERROR HANDLERS
+server.use(error404Handler)
+server.use(error401Handler)
+server.use(error403Handler)
+server.use(genericErrorHandler)
+
+console.log(listEndpoints(server))
+
 server.listen(port, () => console.log("Server is running on port: ", port))
-server.on("error", (error) => console.log("Server is not running ", error))
+server.on("error", (error) => console.log("Server is not running. ", error))
