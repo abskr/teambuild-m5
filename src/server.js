8b8debc197a4ccf61b8165c9cce3e27ee59d112a
ABskr <bskr26@gmx.de>
2021-04-22 16:57:28 +0200

    bugs fixed

--- src/server.js
+++ src/server.js
@@ -3,8 +3,13 @@ import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import productsRoute from "./products/index.js";
 import reviewsRoute from "./reviews/index.js";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
+import {
+  join,
+  dirname
+} from "path";
+import {
+  fileURLToPath
+} from "url";
 import {
   error404Handler,
   error401Handler,
@@ -16,21 +21,29 @@ import swaggerUI from 'swagger-ui-express'
 
 const server = express();
 const port = process.env.PORT || 5000;
-const swaggerDoc = YAML.load(join(dirname(fileURLToPath(import.meta.url))), "./apiDesc.yaml")
+const x = fileURLToPath(
+  import.meta.url)
+const y = dirname(x)
+const z = join(y, "./apiDesc.yml")
+
+console.log({x,y,z})
+const swaggerDoc = YAML.load(z)
 const loggerMiddleware = (req, res, next) => {
   console.log(`Logged ${req.url} ${req.method} -- ${new Date()}`);
   next();
 };
 
 const pathToPublic = join(
-  dirname(fileURLToPath(import.meta.url)),
+  dirname(fileURLToPath(
+    import.meta.url)),
   "../public/img"
 );
 
 server.use(express.static(pathToPublic));
 server.use(cors());
 server.use(express.json());
-
+server.use(loggerMiddleware)
+ 
 server.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
 server.use("/products", productsRoute);
